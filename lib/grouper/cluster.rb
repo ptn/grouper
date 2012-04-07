8a21a2649fe926b1d9a1416a466ff12073794141
Pablo Torres <tn.pablo@gmail.com>
2012-04-07 09:57:32 -0500

    Added Cluster#all_names

--- lib/grouper/cluster.rb
+++ lib/grouper/cluster.rb
@@ -1,9 +1,24 @@
 module Grouper
-
-  Cluster = Struct.new :rankings, :left_cluster, :right_cluster,
-    :children_distance, :name
-
   class Cluster
+    attr_accessor :rankings, :left_cluster, :right_cluster,
+      :children_distance, :name
+
+    def initialize(rankings=nil, left_cluster=nil, right_cluster=nil,
+                   children_distance=nil, name=nil)
+      @rankings = rankings
+      @left_cluster = left_cluster
+      @right_cluster = right_cluster
+      @children_distance = children_distance
+      @name = name
+    end
+
+    def all_names
+      names = @name ? [@name] : []
+      names += @left_cluster.all_names if @left_cluster
+      names += @right_cluster.all_names if @right_cluster
+      names
+    end
+
     def to_s(indent=0)
       if name.nil?
         s = "\n"
@@ -18,5 +33,4 @@ module Grouper
       ("  " * indent) + "(#{indent.to_s})" + s
     end
   end
-
 end
